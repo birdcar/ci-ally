fd05c57eca6223d513121b542c5ad6ada3ddbb5e
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-10-05 17:55:01 -0500

    :bug: Destructure the right object to get the slug

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const axios = require('axios')
  */
 module.exports = app => {
   app.on('check_run.completed', async context => {
-    const { check_run: { details_url, conclusion }, app: { slug: integrationSlug } } = context.payload
-    app.log(integrationSlug)
+    const { check_run: { details_url, conclusion, name: checkName  } } = context.payload
+    app.log(checkName)
     app.log("I ran when I wasn't supposed to")
     if (conclusion === 'failure') {
       const buildId = details_url.split('/').pop()
