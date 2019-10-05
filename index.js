4df90b208397b1ad6b817514ca25b0d49aed21dd
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-10-05 15:32:44 -0500

    :sparkles: Catch check_suite, check_run, and status events

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  app.log('Yay, the app was loaded!')
+
+  app.on('status', async context => {
+    app.log("I'm responding to status")
+  })
+
+  app.on('check_run', async context => {
+    app.log("I'm responding to check_run")
+  })
+
+  app.on('check_suite', async context => {
+    app.log("I'm responding to check_suite")
+  })
+}
