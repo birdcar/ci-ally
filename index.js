dd4e86e5eca005cf775c8ff352395c297909dcb1
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-10-05 18:15:00 -0500

    :art: Modularize and restructure code

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-const axios = require('axios')
+const axios = require('axios');
+
+async function getBuildLog(buildUrl) {
+  // Pull the Travis build ID off of the buildUrl
+  const buildId = buildUrl.split('/').pop();
+
+  // Get the build data from Travis' API
+  const { data } = await axios.get(
+    `https://api.travis-ci.com/v3/build/${buildId}`
+  );
+
+  // Grab the ID of the first job in the jobs array
+  const jobId = data.jobs[0].id;
+
+  // Request the log for that specific job ID
+  const { data: jobLog } = await axios.get(
+    `https://api.travis-ci.com/v3/job/${jobId}/log.txt`
+  );
+
+  // Return the raw text log
+  return jobLog;
+}
+
+function parseLog(jobLog) {
+  // parse the job log for npm test output
+  const reg = /\[0K\$\snpm\stest(?:\r\n|\n)*([\s\S]+)[\r\n]+.*Test failed\./g;
+  const match = reg.exec(jobLog);
+
+  // If no match is found, return false
+  if (!match) {
+    return false;
+  }
+
+  // Trim the output
+  const errors = match[1].trim();
+
+  // Return a new object
+  return {
+    errors,
+    command: 'npm test'
+  };
+}
 
 /**
  * This is the main entrypoint to your Probot app
@@ -7,31 +48,25 @@ const axios = require('axios')
 module.exports = app => {
   app.on('check_run.completed', async context => {
     // Destructure necessary data from the payload
-    const { check_run: { details_url, conclusion, name: checkName  } } = context.payload
-    
-    // Fail fast if the specific check_run isn't Travis CI - Pull Request
-    if (checkName !== "Travis CI - Pull Request") {
-      return false
-    }
+    const {
+      check_run: { details_url: travisBuild, conclusion, name: checkName }
+    } = context.payload;
 
-    app.log('I only run once')
+    // Fail fast if the specific check_run isn't Travis CI - Pull Request
+    if (checkName !== 'Travis CI - Pull Request') {
+      return false;
+    }
 
     // Only take action if the conclusion of the Pull Request check is 'failure'
     if (conclusion === 'failure') {
-      const buildId = details_url.split('/').pop()
-      const { data } = await axios.get(`https://api.travis-ci.com/v3/build/${buildId}`)
-      const jobId = data.jobs[0].id
-      const { data: jobLog } = await axios.get(`https://api.travis-ci.com/v3/job/${jobId}/log.txt`)
-      const reg = /\[0K\$\snpm\stest(?:\r\n|\n)*([\s\S]+)[\r\n]+.*Test failed\./g
-      const result = reg.exec(jobLog)
+      const rawLog = await getBuildLog(travisBuild);
+      const buildResults = parseLog(rawLog);
 
-      if (!result) {
-        return false
+      if (!buildResults) {
+        return null;
       }
 
-      const content = result[1].trim()
-      const obj = { content, command: 'npm test' }
-      app.log(obj)
+      app.log(buildResults);
     }
-  })
-}
+  });
+};
