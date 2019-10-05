edf05d400990a372cb44265ae18fa3645a319d44
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-10-05 16:23:52 -0500

    :hankey: Enable status checks to get travis job

--- index.js
+++ index.js
@@ -5,9 +5,17 @@
 module.exports = app => {
   app.log('Yay, the app was loaded!')
 
+  app.on('status', async context => {
+    app.log("I'm responding to status")
+    const { payload } = context
+    const { owner, repo } = context.repo()
+
+    app.log(owner, repo)
+    app.log(payload)
+  })
+
   app.on('check_run', async context => {
     app.log("I'm responding to check_run")
-    app.log(context)
   })
 
   app.on('check_suite', async context => {
