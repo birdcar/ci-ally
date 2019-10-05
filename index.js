b01304393dd072a9dce99935311cc4f0ac7eba60
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-10-05 17:58:54 -0500

    :sparkles: Fail early if the check isn't Travis CI - Pull Request

--- index.js
+++ index.js
@@ -6,9 +6,17 @@ const axios = require('axios')
  */
 module.exports = app => {
   app.on('check_run.completed', async context => {
+    // Destructure necessary data from the payload
     const { check_run: { details_url, conclusion, name: checkName  } } = context.payload
-    app.log(checkName)
-    app.log("I ran when I wasn't supposed to")
+    
+    // Fail fast if the specific check_run isn't Travis CI - Pull Request
+    if (checkName !== "Travis CI - Pull Request") {
+      return false
+    }
+
+    app.log('I only run once')
+
+    // Only take action if the conclusion of the Pull Request check is 'failure'
     if (conclusion === 'failure') {
       const buildId = details_url.split('/').pop()
       const { data } = await axios.get(`https://api.travis-ci.com/v3/build/${buildId}`)
