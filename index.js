98648ac90679b42454d0dffb3a933e014338a86a
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-10-05 17:45:20 -0500

    :art: Clean up dead code

--- index.js
+++ index.js
@@ -5,29 +5,22 @@ const axios = require('axios')
  * @param {import('probot').Application} app
  */
 module.exports = app => {
-  app.log('Yay, the app was loaded!')
-
   app.on('check_run.completed', async context => {
-    app.log("I'm responding to check_run")
-    const { check_run: { id, details_url, conclusion } } = context.payload
-    const { owner, repo } = context.repo()
-    app.log(id)
-    app.log(details_url)
-    app.log(conclusion)
+    const { check_run: { details_url, conclusion } } = context.payload
+
     if (conclusion === 'failure') {
       const buildId = details_url.split('/').pop()
-      app.log(buildId)
       const { data } = await axios.get(`https://api.travis-ci.com/v3/build/${buildId}`)
       const jobId = data.jobs[0].id
       const { data: jobLog } = await axios.get(`https://api.travis-ci.com/v3/job/${jobId}/log.txt`)
-      app.log(`Check run: ${id}`)
-      app.log(`Repository: ${owner}/${repo}`)
       const reg = /\[0K\$\snpm\stest(?:\r\n|\n)*([\s\S]+)[\r\n]+.*Test failed\./g
       const result = reg.exec(jobLog)
+
       if (!result) {
         return false
       }
-      let content = result[1].trim()
+
+      const content = result[1].trim()
       const obj = { content, command: 'npm test' }
       app.log(obj)
     }
